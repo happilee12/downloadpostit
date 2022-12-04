fd0f44370ea28205f24f77db9b129748c80a26df
한리경 <happilee12@gmail.com>
2022-12-04 16:47:17 +0900

    맥에서 창닫기 시에도  app.quit()

--- src/background.js
+++ src/background.js
@@ -123,9 +123,11 @@ async function createWindow() {
 app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  // if (process.platform !== "darwin") {
+  //   app.quit();
+  // }
+  /** 앱 닫기 시 바로 전체 quit */
+  app.quit();
 });
 
 app.on("activate", () => {
